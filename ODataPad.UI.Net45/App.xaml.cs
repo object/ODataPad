8951456042407ef07e645ba3922701c0fae75478
Vagif Abilov <vagif.abilov@gmail.com>
2013-03-29 12:32:53 +0100

    Correctly wired a view in WPF app.

--- ODataPad.UI.Net45/App.xaml.cs
+++ ODataPad.UI.Net45/App.xaml.cs
@@ -5,9 +5,12 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Cirrious.CrossCore.IoC;
+using Cirrious.MvvmCross.ViewModels;
 using Cirrious.MvvmCross.ExtensionMethods;
 using Cirrious.MvvmCross.Interfaces.ServiceProvider;
 using Cirrious.MvvmCross.Interfaces.ViewModels;
+using Cirrious.MvvmCross.Wpf.Views;
 
 namespace ODataPad.UI.Net45
 {
@@ -16,16 +19,40 @@ namespace ODataPad.UI.Net45
     /// </summary>
     public partial class App : Application, IMvxServiceConsumer
     {
+        private bool _setupComplete = false;
+        
         public App()
         {
             InitializeComponent();
         }
 
-        protected override void OnStartup(StartupEventArgs e)
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    var start = this.GetService<IMvxStartNavigation>();
+        //    start.Start();
+        //}
+        private void DoSetup()
         {
-            base.OnStartup(e);
+            var presenter = new MvxSimpleWpfViewPresenter(MainWindow);
+
+            var setup = new Setup(Dispatcher, presenter);
+            setup.Initialize();
+
+            //var start = Mvx.Resolve<IMvxAppStart>();
+            //start.Start();
             var start = this.GetService<IMvxStartNavigation>();
             start.Start();
+
+            _setupComplete = true;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            if (!_setupComplete)
+                DoSetup();
+
+            base.OnActivated(e);
         }
     }
 }
